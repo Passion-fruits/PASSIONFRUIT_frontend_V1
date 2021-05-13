3bc76bf9ee4e15fb8a2a4afd0bb3e4ceeb044932
jiwon <jidole041214@naver.com>
2021-05-13 11:16:02 +0900

    create : index style 설정

---
+++ src/style/index.tsx
@@ -0,0 +1,9 @@
+export const MAIN_COLOR = "#0090FF";
+
+export const BACKGROUND_COLOR = "#191919";
+
+export const FONT_COLOR = "#A2A2A2";
+
+export const YELLOW_COLOR = "#FFEE00";
+
+export const ORANGE_COLOR = "#FF8000";
