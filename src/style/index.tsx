3b230bb4abfe9b3ffecee4cb71115dffc313059b
jiwon <jidole041214@naver.com>
2021-05-17 10:29:59 +0900

    add : index style

--- src/style/index.tsx
+++ src/style/index.tsx
@@ -6,4 +6,8 @@ export const COLOR = {
     ORANGE : "#FF8000"
 }
 
-export const PADDING = "200px";
+export const WIDTH = "1450px";
+
+export const TABLET_WIDTH = "1024px";
+
+export const PHONE_WIDTH = "768px";
