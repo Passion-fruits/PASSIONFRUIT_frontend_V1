60fcc2e0764a942bd28b27cc7300f48630d85804
jiwon <jidole041214@naver.com>
2021-05-19 14:16:47 +0900

    create : feed랑 streaming 쪽 서브헤더

---
+++ src/components/subHeader/index.tsx
@@ -0,0 +1,19 @@
+import { SubHeaderParams } from "../../interfaces/feed";
+import * as s from "./styles";
+
+export default function SubHeader({title,description}:SubHeaderParams) {
+  return (
+    <s.FullWrapper>
+      <s.Wrapper>
+        <s.Container>
+          <s.Title>passion fruit</s.Title>
+          <s.Description>: music highlights</s.Description>
+        </s.Container>
+        <s.Container>
+          <s.Title>{title}</s.Title>
+          <s.Description>{description}</s.Description>
+        </s.Container>
+      </s.Wrapper>
+    </s.FullWrapper>
+  );
+}
