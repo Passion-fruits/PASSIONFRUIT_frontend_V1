92a069fa0f8e92104ddd8b8ac0e6889edb092b5b
Sung Ye In <syi9397@naver.com>
2021-05-20 08:44:18 +0900

    create : streaming card styles

---
+++ src/components/public/StreamingCard/styles.ts
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+export const CardWrapper = styled.div`
+    width: 258px;
+    height: 370px;
+    border-radius: 6px;
+    background: #191919;
+    padding: 20px;
+`;
+
+export const CardImg = styled.img`
+    width: 218px;
+    height: 218px;
+    object-fit: cover;
+`;
+
+export const CardTitle = styled.h2`
+    color: white;
+    font-size: 19px;
+    font-weight: bold;
+`;
+
+export const CardUserIcon = styled.img`
+    border-radius: 50%;
+    width: 23px;
+    height: 23px;
+    object-fit: cover;
+`;
+
+export const CardUserName = styled.h3`
+    color: #9d9d9d;
+    font-size: 14px;
+    font-weight: bold;
+`;
