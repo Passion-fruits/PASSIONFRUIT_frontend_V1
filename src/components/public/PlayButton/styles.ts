7b6bd44c853d69ac3cc88ac4e773f062e6d78856
Sung Ye In <syi9397@naver.com>
2021-05-20 21:33:04 +0900

    create : playbutton

---
+++ src/components/public/PlayButton/styles.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+import { ButtonInitStyle } from "../../../style";
+
+export const PlayButton = styled.button`
+    ${ButtonInitStyle}
+    width: 76px;
+    height: 76px;
+    border-radius: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background: #008ffe;
+`;
