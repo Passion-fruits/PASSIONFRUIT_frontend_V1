84ba10d5bc40c1c698fb91426ca66248c1277018
jiwon <jidole041214@naver.com>
2021-05-20 19:04:10 +0900

    create : recommend component

---
+++ src/components/feed/recommend/styles.tsx
@@ -0,0 +1,29 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.div`
+display: flex;
+flex-direction:column;
+align-items:flex-end;
+width:200px;
+position:sticky;
+top:0px;
+`
+
+export const Title = styled.span`
+color:white;
+font-weight:600;
+font-size:15px;
+`
+
+export const Refresh = styled.button`
+background:rgb(50,50,50);
+border:none;
+padding: 3px 15px;
+border-radius:4px;
+color:gray;
+margin-top:10px;
+i{
+    color:gray;
+    font-size:13px;
+}
+`
