a1465ec7949da47d2c5468b99fdabcdaa466f946
Sung Ye In <syi9397@naver.com>
2021-05-20 21:33:57 +0900

    update : button init style

--- src/style/index.tsx
+++ src/style/index.tsx
@@ -1,13 +1,20 @@
 export const COLOR = {
-    MAIN_COLOR : "#0090FF",
-    BACKGROUND_COLOR : "#191919",
-    FONT_COLOR : "#A2A2A2",
-    YELLOW : "#FFEE00",
-    ORANGE : "#FF8000"
-}
+    MAIN_COLOR: "#0090FF",
+    BACKGROUND_COLOR: "#191919",
+    FONT_COLOR: "#A2A2A2",
+    YELLOW: "#FFEE00",
+    ORANGE: "#FF8000",
+};
 
 export const WIDTH = "1450px";
 
 export const TABLET_WIDTH = "1024px";
 
 export const PHONE_WIDTH = "768px";
+
+export const ButtonInitStyle = `
+    outline: none;
+    border: none;
+    background: none;
+    cursor: pointer;
+`;
