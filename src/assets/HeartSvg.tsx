b959897f57af6b9804901c1952067356d178f7e6
Sung Ye In <syi9397@naver.com>
2021-05-21 10:41:36 +0900

    create : svgs

---
+++ src/assets/HeartSvg.tsx
@@ -0,0 +1,22 @@
+const HeartSvg = () => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="15.781"
+        height="16.345"
+        viewBox="0 0 15.781 16.345"
+    >
+        <path
+            id="Icon_feather-heart"
+            data-name="Icon feather-heart"
+            d="M14.118,5.677a2.987,2.987,0,0,0-4.757,0l-.648.776-.648-.776a2.988,2.988,0,0,0-4.757,0,4.607,4.607,0,0,0,0,5.695l.648.776,4.757,5.695,4.757-5.695.648-.776a4.606,4.606,0,0,0,0-5.695Z"
+            transform="translate(-0.823 -2.997)"
+            fill="none"
+            stroke="#fff"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="3"
+        />
+    </svg>
+);
+
+export default HeartSvg;
---
+++ src/assets/index.ts
@@ -0,0 +1,5 @@
+export { default as PlaySvg } from "./PlaySvg";
+export { default as PauseSvg } from "./PauseSvg";
+export { default as TalkSvg } from "./TalkSvg";
+export { default as HeartSvg } from "./HeartSvg";
+export { default as CloudSvg } from "./CluodSvg";
