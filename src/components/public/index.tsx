0e4e0fb46fb296d1466d230a3e3b18cc6a4dd2b1
jiwon <jidole041214@naver.com>
2021-05-23 21:46:46 +0900

    create : routing index.tsx

---
+++ src/components/public/index.tsx
@@ -0,0 +1,5 @@
+export { default as SignIn } from "../../components/auth/signin";
+export { default as SignUp } from "../../components/auth/signup";
+export { default as Feed } from "../../components/feed";
+export { default as Header } from "../../components/header";
+export { default as Upload } from "../../components/upload";
