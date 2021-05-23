7693e30928e23d47ff9d3ec1585953b13cd35e01
jiwon <jidole041214@naver.com>
2021-05-23 21:46:58 +0900

    update : import from index.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Route, Switch } from "react-router";
 import { BrowserRouter as Router } from "react-router-dom";
-import SignIn from "./components/auth/signin";
-import SignUp from "./components/auth/signup";
-import Feed from "./components/feed";
-import Header from "./components/header";
-import Upload from "./components/upload";
 import GlobalStyle from "./style/globalStyle";
+import {
+  Header,
+  SignIn,
+  SignUp,
+  Feed,
+  Upload,
+} from "./components/public/index";
 
 function App() {
   return (
