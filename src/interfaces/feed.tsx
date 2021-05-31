286bfac6fe382cdf1424219eceecd67f6c589869
jiwon <jidole041214@naver.com>
2021-05-31 11:19:28 +0900

    add : interfaces

--- src/interfaces/feed.tsx
+++ src/interfaces/feed.tsx
@@ -1,29 +1,56 @@
-export interface SubHeaderParams{
-    title : string;
-    description : string;
+export interface SubHeaderParams {
+  title: string;
+  description: string;
 }
 
-export interface OptionParams{
-    dropDown : boolean;
-    option : string;
-    indexNum : number;
-    setSelectVal : any;
-    nowOption : string;
+export interface OptionParams {
+  dropDown: boolean;
+  option: string;
+  indexNum: number;
+  setSelectVal: any;
+  nowOption: string;
 }
 
-export interface SelectParams{
-    controlDropDown : any;
-    dropDownBool : boolean;
-    arr : any[];
-    selectTitle : string;
-    title : string;
-    AllOption : any;
+export interface SelectParams {
+  controlDropDown: any;
+  dropDownBool: boolean;
+  arr: any[];
+  selectTitle: string;
+  title: string;
+  AllOption: any;
 }
 
 export interface WaveParams {
-    second: number;
+  second: number;
 }
 
-export interface ProgressParams{
-    width:number;
+export interface ProgressParams {
+  width: number;
 }
+
+export interface CoverImgProps {
+  cover: string;
+}
+
+export interface Infor {
+  title: string;
+  genre: string;
+  date: string;
+  description: string;
+}
+
+export interface MusicInforProps extends Infor {
+  playBool: boolean;
+}
+
+export interface ProfileProps {
+  profile: string;
+  name: string;
+}
+
+export interface FeedCardProps extends Infor, CoverImgProps, ProfileProps {
+  func(e: string): void;
+  nowPlay: string;
+}
+
+export interface MusicObject extends ProfileProps, Infor, CoverImgProps {}
